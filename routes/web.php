87876cb2d04694cfba5029921ed718b79610bf45
flayer0 <a.karack4iev@gmail.com>
2025-11-05 19:44:30 +0300

    3. Подгружаем фронт страницы блога. Список постов

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController')->name('main.index');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
