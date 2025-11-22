721376e0b596336aa4cfec55698f6db2a98bc2cf
flayer0 <a.karack4iev@gmail.com>
2025-11-22 09:27:26 +0300

    5. Определяемся со стратегией и создаем задел для CRUD категорий

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -47,16 +47,7 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+  @include('admin.includes.sidebar')
 
   @yield('content')
   <!-- /.content-wrapper -->
