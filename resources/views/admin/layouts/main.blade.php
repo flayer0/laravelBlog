11a253bc4599819f5a9bc101e1ca2fb4a6fd7318
flayer0 <a.karack4iev@gmail.com>
2025-11-29 09:33:06 +0300

    17. Добавляем возможность привязывать пост к категории

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -123,6 +123,11 @@
             bsCustomFileInput.init();
         });
     </script>
+    <style>
+        .custom-file-input:lang(en)~.custom-file-label::after {
+            content: "...";
+        }
+    </style>
 </body>
 
 </html>
--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -38,7 +38,7 @@
                             </div>
                             <div class="form-group">
                                 <textarea name="content" id="summernote">{{ old('title') }}</textarea>
-                                @error('title')
+                                @error('content')
                                     <div class="text-danger">Это поле необходимо для заполнение</div>
                                 @enderror
                             </div>
@@ -53,6 +53,9 @@
                                         <span class="input-group-text">Upload</span>
                                     </div>
                                 </div>
+                                @error('preview_image')
+                                    <div class="text-danger">Это поле необходимо для заполнение</div>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputFile">Добавить изображение</label>
@@ -65,6 +68,23 @@
                                         <span class="input-group-text">Upload</span>
                                     </div>
                                 </div>
+                                @error('main_image')
+                                    <div class="text-danger">Это поле необходимо для заполнение</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Выберите категорию</label>
+                                <select class="form-control" name="category_id">
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $cayegory->id == old('category_id') ? 'selected' : '' }}>
+                                            {{ $category->title }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
+                                    <div class="text-danger">Это поле необходимо для заполнение</div>
+                                @enderror
                             </div>
                             <input type="submit" value="Добавить" class="btn btn-primary">
                         </form>
